39beddf4f8662893659377b7850ae1da83d4eddc
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-06-05 20:03:23 +0200

    first commit

---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      {/* Add your registration form or components here */}
+    </div>
+  );
+};
+
+export default RegisterPage;
