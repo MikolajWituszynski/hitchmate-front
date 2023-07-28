42921aef15fdae242cd2a99988f47d15adeb9970
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-07-28 17:29:09 +0200

    include react component as componenet info window

---
+++ src/components/MapPage/MarkerMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const markerMenu = () => {
+  return (
+        <div>
+            <button>Edit Marker</button>
+            <button>Delete Marker</button>
+        </div>
+  )
+}
+
+export default markerMenu
