55725c7a347a70b81ffd533d2219ccd87c2b19cd
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-07-28 17:32:30 +0200

    infowindow with react component

--- src/components/MapPage/Map.js
+++ src/components/MapPage/Map.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import loadGoogleMapsApi from './loadGoogleMapsApi';
-
+import ReactDOM from 'react-dom';
+import MarkerMenu from './MarkerMenu.js';
 const Map = ({ apiKey, lat, lng, zoom }) => {
   const mapContainerRef = useRef(null);
-  const [isEditOpen, setEditOpen] = useState(false);
 
   useEffect(() => {
     loadGoogleMapsApi(apiKey)
@@ -15,19 +15,6 @@ const Map = ({ apiKey, lat, lng, zoom }) => {
         
         let markers = [];
 
-        const editMarker = (marker) => {
-          console.log("edit", marker);
-          const editWindow = new google.maps.InfoWindow({
-            content: `
-              <div id="content">
-                <p><button>Description</button></p>
-                <p><button>Photos</button></p>
-              </div>`
-          });
-          editWindow.open(map,marker);
-
-        }
-
         const deleteMarker = (marker) => {
           marker.setMap(null);
         }
@@ -38,32 +25,26 @@ const Map = ({ apiKey, lat, lng, zoom }) => {
             position: e.latLng,
             map: map,
           });
-
-          const markerId = Math.random().toString(36).substring(2, 15); // Generate a unique id for the marker
-
-          const infoWindow = new google.maps.InfoWindow({
-            content: `
-              <div id="content">
-                <p><button id="edit-${markerId}">Edit Marker</button></p>
-                <p><button id="delete-${markerId}">Delete Marker</button></p>
-              </div>`
-          });
-
+          
           newMarker.addListener("rightclick", (e) => {
-            infoWindow.open(map, newMarker);
-            // if(editMarker) {
-            //   infoWindow.close(map, newMarker)
-            // }
+            const markerInfoWindow = new google.maps.InfoWindow({
+               content: '<div id="content"></div>',
+              ariaLabel: "Uluru",
+            });
+            markerInfoWindow.open({
+              anchor:newMarker,
+               map,
+            });
+            google.maps.event.addListener(markerInfoWindow, 'domready', function() {
+              ReactDOM.render(<MarkerMenu />, document.getElementById('content'));
           });
-
-          // Add listeners to buttons when InfoWindow's dom is ready
-          google.maps.event.addListener(infoWindow, 'domready', () => {
-            document.getElementById(`edit-${markerId}`).addEventListener('click', () => editMarker(newMarker));
-            document.getElementById(`delete-${markerId}`).addEventListener('click', () => deleteMarker(newMarker));
-          });
+          })
+          
 
           markers.push(newMarker);
         });
+
+        
         
       })
       .catch(error => {
