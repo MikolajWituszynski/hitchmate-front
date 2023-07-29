6a4a0ea93dca3ba2e16cb27eebc1bbaf0f32bd09
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-07-29 19:11:19 +0200

    addition of marker info component

--- src/components/MapPage/Map.js
+++ src/components/MapPage/Map.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import loadGoogleMapsApi from './loadGoogleMapsApi';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import MarkerMenu from './MarkerMenu.js';
+import MarkerInfo from './MarkerInfo';
 import { createRoot } from 'react-dom/client';
 
 const Map = ({ apiKey, lat, lng, zoom }) => {
   const mapContainerRef = useRef(null);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [markerPixelPosition, setMarkerPixelPosition] = useState(null);
+
+  console.log("Here is the selected marker: ", selectedMarker); // Moved here
+
+  const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     loadGoogleMapsApi(apiKey)
@@ -14,28 +22,44 @@ const Map = ({ apiKey, lat, lng, zoom }) => {
           center: { lat, lng },
           zoom,
         });
-        
-        let markers = [];
-        let markerCounter = 0; // Use a simple variable instead of a state
 
-        map.addListener("click", (e) => {
+        let markerCounter = 0;
+        let mapClickListener, markerClickListener, markerRightClickListener, domReadyListener;
+        let markerInfoWindow;
+
+        mapClickListener = map.addListener("click", (e) => {
           const newMarker = new google.maps.Marker({
-            position: e.latLng,
+            position: {
+              lat: e.latLng.lat(),
+              lng: e.latLng.lng(),
+            },
             map: map,
           });
-            
-          const markerId = `content-${markerCounter}`; // Create a unique id for this marker
 
-          const markerInfoWindow = new google.maps.InfoWindow({
-            // Use the unique id for this marker's content
+          const markerId = `content-${markerCounter}`;
+
+          markerInfoWindow = new google.maps.InfoWindow({
             content: `<div id="${markerId}"></div>`,
           });
 
-          newMarker.addListener("rightclick", (e) => {
+          markerClickListener = newMarker.addListener("click", (e) => {
+            console.log("Marker clicked!");
+
+            setSelectedMarker(newMarker);
+
+          })
+          markerClickListener = newMarker.addListener("dbclick", (e) => {
+            console.log("Marker clicked!");
+
+            setSelectedMarker(null);
+
+          })
+
+          markerRightClickListener = newMarker.addListener("rightclick", (e) => {
             markerInfoWindow.open(map, newMarker);
           });
 
-          google.maps.event.addListener(markerInfoWindow, 'domready', () => {
+          domReadyListener = google.maps.event.addListener(markerInfoWindow, 'domready', () => {
             const contentNode = document.getElementById(markerId);
             let root = contentNode._reactRootContainer;
 
@@ -46,10 +70,20 @@ const Map = ({ apiKey, lat, lng, zoom }) => {
               root.render(<MarkerMenu />);
             }
           });
-          
-          markers.push(newMarker);
-          markerCounter++; // increment the counter each time a new marker is added
+
+          setMarkers(prevMarkers => [...prevMarkers, newMarker]);
+          console.log("Here is the selected markers: ", markers); // Moved here
+
+          markerCounter++;
         });
+
+        return () => {
+          google.maps.event.removeListener(mapClickListener);
+          google.maps.event.removeListener(markerClickListener);
+          google.maps.event.removeListener(markerRightClickListener);
+          google.maps.event.removeListener(domReadyListener);
+          markerInfoWindow.close();
+        };
       })
       .catch(error => {
         console.error('Error occurred while loading Google Maps API: ', error);
@@ -59,7 +93,12 @@ const Map = ({ apiKey, lat, lng, zoom }) => {
       });
   }, [apiKey, lat, lng, zoom]);
 
-  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+  return (
+    <div>
+      {selectedMarker && <MarkerInfo lat={selectedMarker.lat} lng={selectedMarker.lng} />}
+      <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
+    </div>
+  );
 };
 
 export default Map;
