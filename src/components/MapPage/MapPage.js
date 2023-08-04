b63c1874b0423fdbdb97bc18f35100e2b1c6e9ef
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-08-04 18:56:00 +0200

    change coordinates

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -5,7 +5,7 @@ const MapPage = () => {
     let googleApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   return (
   <div>
-    <Map apiKey={googleApiKey} lat={50.7128} lng={44.0060} zoom ={10}></Map>
+    <Map apiKey={googleApiKey} lat={22.7128} lng={12.0060} zoom ={10}></Map>
     </div>
   )
 }
