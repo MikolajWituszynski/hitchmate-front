4f94b2e4ac8a4411513005642cf120297ae4d28c
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-08-16 10:59:17 +0200

    fixed problem with props drilling

--- src/components/MapPage/MarkerInfo.js
+++ src/components/MapPage/MarkerInfo.js
@@ -1,34 +1,30 @@
-// MarkerInfo.js
+import React, {useState} from 'react';
+import { useMarkerContext } from './MarkerContext';
 
-const MarkerInfo = ({ lat, lng, style, title, description, onClose }) => {
+const MarkerInfo = ({ style, onClose, description, title,lat,lng }) => {
+  const { markers, updateMarker, currentId } = useMarkerContext();
+
+  
   return (
     <div>
-    
-        <div
-          className="bg-blue-500 rounded p-2 my-2"
-          style={{
-            ...style,
-            position: 'absolute',
-            backgroundColor: 'white',
-            zIndex: 1000,
-            display:"flex"
-          }}
-        >
-          <button
-            className="ml-auto"
-            onClick={onClose}
-            style={{ cursor: 'pointer' }}
-            
-          >
-            x
-          </button>
-          <div className="bg-blue-500 text-white rounded p-2 my-2">Title: {title}</div>
-          <div className="bg-blue-500 text-white rounded p-2 my-2">Description: {description}</div>
-          <div className="bg-blue-500 text-white rounded p-2 my-2">Latitude: {Math.round(lat*100)/100}</div>
-          <div className="bg-blue-500 text-white rounded p-2 my-2">Longitude: {Math.round(lng*100)/100}</div>
-          
-        </div>
-  
+      <div
+        className="bg-blue-500 rounded p-2 my-2"
+        style={{
+          ...style,
+          position: 'absolute',
+          backgroundColor: 'white',
+          zIndex: 1000,
+          display: 'flex',
+        }}
+      >
+        <button className="ml-auto" onClick={onClose} style={{ cursor: 'pointer' }}>
+          x
+        </button>
+        <div className="bg-blue-500 text-white rounded p-2 my-2">Title: { title}</div>
+        <div className="bg-blue-500 text-white rounded p-2 my-2">Description: { description}</div>
+        <div className="bg-blue-500 text-white rounded p-2 my-2">Latitude: {lat}</div>
+        <div className="bg-blue-500 text-white rounded p-2 my-2">Longitude: {lng}</div>
+      </div>
     </div>
   );
 };
