a0974ccc6cc832a7a9e8f28610dfd779a3c51201
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-08-16 11:29:26 +0200

    update

--- src/components/MapPage/MarkerMenu.js
+++ src/components/MapPage/MarkerMenu.js
@@ -1,63 +1,41 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useMarkerContext } from './MarkerContext';
 
-const MarkerMenu = ({ setMarkerDescription , setMarkerTitle, title, description, style,onClose}) => {
-  const[isEditShown, setEditIsShown] = useState(false)
-  const [newDescription, setNewDescription] = useState(description || "");
-  const [newTitle, setNewTitle] = useState(title || "");
-  const handleEditClick = (event) => {
-    event.preventDefault();
-    setEditIsShown(true);
-    
-  }
-
-  const handleDeleteClick = () => {
-    // Add your logic for handling deleting a marker
-  }
+const MarkerMenu = ({ id, style, onClose }) => {
+  const { markers, updateMarker, currentId } = useMarkerContext();
+  const markerData = markers.find(marker => marker.id === id);
+  // if (!markerData) {
+  //   return null; // Handle case where marker is not found
+  // }
+  // Local state for title and description
+  const [newDescription, setNewDescription] = useState(markerData.description || "");
+  const [newTitle, setNewTitle] = useState(markerData.title || "");
 
   const handleSave = event => {
     event.preventDefault();
-    setMarkerTitle(newTitle);
-    setMarkerDescription(newDescription);  
-    setEditIsShown(false);
-
+    updateMarker(id, { title: newTitle, description: newDescription });
+    console.log(updateMarker)
+  
+    onClose();
   };
 
   return (
-        <div  style={{
-          ...style,
-          position: 'absolute',
-          backgroundColor: 'white',
-          zIndex: 1000,
-          display:"flex"
-        }} >
-          <button
-            className="ml-auto"
-            onClick={onClose}
-            style={{ cursor: 'pointer' }}
-            
-          >
-            x
-          </button>
-        {!isEditShown ? 
-          <div className="flex flex-col">
-        <button className="bg-blue-500 text-white rounded p-2 my-2" onClick={handleEditClick}>Edit Marker</button>
-        <button className="bg-blue-500 text-white rounded p-2 my-2" onClick={handleDeleteClick}>Delete Marker</button>
-        </div> : 
-        <div >
-        <form className="flex flex-col">
-          <label>
-              Title:
-              <input className="border " type="text" name="name" value={newTitle} onChange={(e) =>{ setNewTitle(e.target.value)}}/>
-          </label> <label>
-              Description:
-              <input className="border" type="text" name="name" value={newDescription} onChange={(e) => {setNewDescription(e.target.value)  }}/>
-         </label>
-         <button onClick={handleSave}>Save</button>
-        </form>
-        </div>}
-        </div>
-  )
-}
+    <div style={{ ...style, position: 'absolute', backgroundColor: 'white', zIndex: 1000, display: "flex" }}>
+      <button className="ml-auto" onClick={onClose} style={{ cursor: 'pointer' }}>x</button>
+      
+      <div className="flex flex-col">
+        <label>
+          Title:
+          <input className="border" type="text" name="name" value={newTitle} onChange={e => setNewTitle(e.target.value)} />
+        </label>
+        <label>
+          Description:
+          <input className="border" type="text" name="name" value={newDescription} onChange={e => setNewDescription(e.target.value)} />
+        </label>
+        <button onClick={handleSave}>Save</button>
+      </div>
+    </div>
+  );
+};
 
 export default MarkerMenu;
