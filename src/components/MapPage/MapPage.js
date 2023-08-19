83273a0171d24ac162643b0fa3a451c7dbda8df0
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-08-19 09:45:39 +0200

    addition of navbar to mapPage component

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Map from './Map'
 import axios from 'axios'
+import NavBar from '../layout/NavBar/NavBar';
 const MapPage = () => {
     let googleApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   
   return (
   <div>
+    <NavBar/>
     <Map apiKey={googleApiKey} lat={46.7128} lng={6.0060} zoom ={10}></Map>
     </div>
   )
